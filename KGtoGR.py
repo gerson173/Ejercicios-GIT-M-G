f56d086ce49f08f124efdc4b58e1a99a33ad16f7
gerson173 <107493460+gerson173@users.noreply.github.com>
2022-08-23 18:48:10 -0500

    Final

--- KGtoGR.py
+++ KGtoGR.py
@@ -2,4 +2,4 @@ import os
 
 Kilogramo=float (input ('Ingresar el valor en kilogramos (KG): '))
 gramo=1000*Kilogramo
-print('Equivale a: ' + repr(gramo) + ' la cantidad en gramos es ')
+print('Equivale a: ' + repr(gramo) + ' la cantidad en gramos es: ')
