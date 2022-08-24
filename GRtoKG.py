00a65cd9ca7fafcea33bb9acb750a8558dcaa47c
gerson173 <107493460+gerson173@users.noreply.github.com>
2022-08-23 19:02:48 -0500

    main

--- GRtoKG.py
+++ GRtoKG.py
@@ -2,4 +2,4 @@ import os
 
 gramo=float (input ('Hola mi pana ingresa el valor en gramos: '))
 kilogramo=gramo/1000
-print('Equivale a: ' + repr(kilogramo) + ' Kilogramos mi estimado culiao')
+print('Equivale a: ' + repr(kilogramo) + ' Kilogramos mi estimado')
